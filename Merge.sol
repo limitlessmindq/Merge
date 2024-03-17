52c15b615419dc489458180ff420dedccb9ecadf
limitlessmindq <150233268+limitlessmindq@users.noreply.github.com>
2024-03-17 15:12:15 +0300

    Update Merge.sol

--- Merge.sol
+++ Merge.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import { OFT } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract Merge is OFT, ERC20Permit {
+    constructor(
+        uint256 _totalSupply, // token totalSupply
+        address _layerZeroEndpoint // local endpoint address
+    ) OFT("Merge", "MERGE", _layerZeroEndpoint, msg.sender) ERC20Permit("Merge") Ownable(msg.sender) {
+        _mint(msg.sender, _totalSupply); 
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
